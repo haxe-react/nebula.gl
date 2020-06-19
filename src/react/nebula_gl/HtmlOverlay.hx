08be4dcb91eaa28c03ef645e0427ebf675a9cadc
Kevin Leung <kevinresol@gmail.com>
2020-06-19 15:56:57 +0800

    Add HtmlOverlay

---
+++ src/react/nebula_gl/HtmlOverlay.hx
@@ -0,0 +1,9 @@
+package react.nebula_gl;
+
+import react.ReactComponent;
+
+@:jsRequire('@nebula.gl/overlays', 'HtmlOverlay')
+extern class HtmlOverlay extends ReactComponentOfProps<{children:ReactFragment}> {}
+
+@:jsRequire('@nebula.gl/overlays', 'HtmlOverlayItem')
+extern class HtmlOverlayItem extends ReactComponentOfProps<{children:ReactFragment, coordinates:tink.s2d.Point}> {}
